9794a2e79e06f2d86b899908527687d3e33d2c1b
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-18 20:25:37 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/Header';
+import InfoPage from './components/info/InfoPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <InfoPage />
     </div>
   );
 }
---
+++ src/components/info/InfoPage.jsx
@@ -0,0 +1,11 @@
+import './InfoPage.css';
+
+const InfoPage = () => {
+  return (
+    <section className="info__page">
+     
+    </section>
+  );
+}
+
+export default InfoPage;
