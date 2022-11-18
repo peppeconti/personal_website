092a9fd60a9b2c988e395a6eefb191365aabe5f2
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-18 20:00:20 +0100

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,10 +10,11 @@ const Header = () => {
 
   const header = useRef();
   const tl = useRef();
+  const tla = useRef();
 
   useLayoutEffect(() => {
 
-    tl.current = gsap.timeline()
+    tl.current = gsap.timeline();
 
     const ctx = gsap.context(() => {
       tl.current
@@ -23,11 +24,19 @@ const Header = () => {
             each: .5
           }
         })
-        .to('.text', { duration: .6, y: '0', opacity: 1, ease: 'easeOut', stagger: .5 })
+        .to('.text', { duration: .6, opacity: 1, ease: 'easeOut', stagger: .5 })
         .to('.nav ul li', {
-          duration: .6, y: '0', opacity: 1, ease: 'easeOut', stagger: .5 },"-=.6")
+          duration: .6, opacity: 1, rotateY: 0, onStart: () => {
+            tla.current = gsap.timeline().to('.nav ul li img', {delay: .25, duration: .6, opacity: 1, ease: 'easeOut', stagger: .5 });
+          }, ease: 'easeOut', stagger: .5
+        }, "-=.6")
         .to('.footer__header ul li', {
-          duration: .6, y: '0', opacity: 1, ease: 'easeOut', stagger: {
+          duration: .6, opacity: 1, rotateY: 0, onStart: () => {
+            tla.current = gsap.timeline().to('.footer__header ul li img', {delay: .25, duration: .6, opacity: 1, ease: 'easeOut', stagger: {
+              from: "end",
+              each: .5
+            }});
+          }, ease: 'easeOut', stagger: {
             from: "end",
             each: .5
           }
