7f778080e13a5a06154fd8e613535f1a35093f50
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-20 22:38:18 +0100

    update

--- src/components/about/main/MainAbout.jsx
+++ src/components/about/main/MainAbout.jsx
@@ -2,13 +2,15 @@ import classes from './MainAbout.module.scss';
 
 const MainAbout = () => {
 
-  return (
-    <div className={classes.main__about}>
-        <h1>About me</h1>
-        <aside className={classes.img__profile}></aside>
-        <div className={classes.introduction}></div>
-    </div>
-  );
+    return (
+        <div className={classes.main__about}>
+            <h1>About me</h1>
+            <div className={classes.flex}>
+                <aside className={classes.img__profile}></aside>
+                <div className={classes.introduction}></div>
+            </div>
+        </div>
+    );
 }
 
 export default MainAbout;
