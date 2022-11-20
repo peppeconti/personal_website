69aff70c50eca35c87d74aca4f1061683009c3bf
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-20 22:46:18 +0100

    update

--- src/components/about/main/MainAbout.jsx
+++ src/components/about/main/MainAbout.jsx
@@ -6,8 +6,12 @@ const MainAbout = () => {
         <div className={classes.main__about}>
             <h1>About me</h1>
             <div className={classes.flex}>
-                <aside className={classes.img__profile}></aside>
-                <div className={classes.introduction}></div>
+                <aside className={classes.img__profile}>
+                    cioa
+                </aside>
+                <div className={classes.introduction}>
+                    aloha
+                </div>
             </div>
         </div>
     );
