6d7d2e07a9e3ee46196f4202bcea042ff017e531
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-20 01:03:02 +0100

    update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,17 @@
 import './Portfolio.css';
 import Nav from '../home/nav/Nav';
+import { motion as m } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 const Showcase = () => {
   return (
-    <section className="portfolio">
-     <Nav />
-     <h1>Portfolio</h1>
-    </section>
+    <m.section className="portfolio" exit={{ opacity: 0 }} transition={{ duration: 2 }}>
+      <Nav />
+      <h1>Portfolio</h1>
+      <Link to='/'>
+        <button>Home</button>
+      </Link>
+    </m.section>
   );
 }
 
