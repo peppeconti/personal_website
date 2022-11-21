0a9b095cfa3646e5f65a191450c4f89321ecf059
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-21 20:32:48 +0100

    update

--- src/components/about/main/Biographic.jsx
+++ src/components/about/main/Biographic.jsx
@@ -9,10 +9,26 @@ const Biographic = () => {
     return (
         <article className={classes.bio}>
             <p className={classes.about__me}>
-
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </p>
             <div className={classes.skills}>
-
+                <h2><span>Skills</span></h2>
+                <div className={classes.logos}>
+                    <ul>
+                        <li>
+                            <img src={js__logo} alt="javascript logo" />
+                        </li>
+                        <li>
+                            <img src={react__logo} alt="react logo" />
+                        </li>
+                        <li>
+                            <img src={mongodb__logo} alt="mongodb logo" />
+                        </li>
+                        <li>
+                            <img src={angular__logo} alt="angular logo" />
+                        </li>
+                    </ul>
+                </div>
             </div>
         </article>
     );
--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router-dom';
 import classes from './Nav.module.scss';
 import LinkIcon from './LinkIcon';
 import envelope from '../assets/envelope-solid.svg';
@@ -6,9 +7,13 @@ import laptop from '../assets/laptop-code-solid.svg';
 
 const Nav = () => {
 
+  const location = useLocation();
+
+  const page = location.pathname !== '/' ? classes.page : undefined;
+
   return (
     <nav className={classes.nav}>
-      <ul>
+      <ul className={page}>
         <LinkIcon href={'/about'} source={info} alt="about me" isRoute/>
         <LinkIcon href={'/portfolio'} source={laptop} alt="portfolio" isRoute/>
         <LinkIcon href={'/contact'} source={envelope} alt="contact me" isRoute/>
