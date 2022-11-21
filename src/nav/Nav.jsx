f1e952ae57d9ab75a87b7874537cb02c28565a66
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-21 10:15:28 +0100

    update

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -1,4 +1,3 @@
-import { useLocation } from 'react-router-dom';
 import classes from './Nav.module.scss';
 import LinkIcon from './LinkIcon';
 import envelope from '../assets/envelope-solid.svg';
@@ -7,13 +6,9 @@ import laptop from '../assets/laptop-code-solid.svg';
 
 const Nav = () => {
 
-  const location = useLocation();
-
-  const listClass =  location.pathname !== '/' ? classes.page : undefined;
-
   return (
     <nav className={classes.nav}>
-      <ul className={listClass}>
+      <ul>
         <LinkIcon href={'/about'} source={info} alt="about me" isRoute/>
         <LinkIcon href={'/portfolio'} source={laptop} alt="portfolio" isRoute/>
         <LinkIcon href={'/contact'} source={envelope} alt="contact me" isRoute/>
