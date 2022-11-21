d4edb80563f37fe33fe6ea1daa10290438569e3e
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-21 22:20:59 +0100

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,16 @@
 import classes from './About.module.scss';
+import home from '../../assets/home.svg';
 import Nav from '../../nav/Nav';
+import LinkIcon from '../../nav/LinkIcon';
 import MainAbout from './main/MainAbout';
 
 const About = () => {
 
   return (
     <section className={classes.about}>
-        <Nav/>
-        <MainAbout/>
+      <Nav />
+      <h1><span>About me</span></h1>
+      <div className={classes.home__icon}><LinkIcon href={'/'} source={home} alt="home" isRoute/></div>
     </section>
   );
 }
