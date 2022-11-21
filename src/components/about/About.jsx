ba7ea8bae299772def61db84f0655be5c134d756
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-21 23:20:58 +0100

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,16 +2,20 @@ import classes from './About.module.scss';
 import home from '../../assets/home.svg';
 import Nav from '../../nav/Nav';
 import LinkIcon from '../../nav/LinkIcon';
-import MainAbout from './main/MainAbout';
+import { motion as m } from 'framer-motion';
+import Biographic from './main/Biographic';
+import Skills from './main/Skills';
 
 const About = () => {
 
   return (
-    <section className={classes.about}>
+    <m.section className={classes.about} exit={{ opacity: 0 }} transition={{ duration: 2 }}>
       <Nav />
       <h1><span>About me</span></h1>
-      <div className={classes.home__icon}><LinkIcon href={'/'} source={home} alt="home" isRoute/></div>
-    </section>
+      <div className={classes.home__icon}><LinkIcon href={'/'} source={home} alt="home" isRoute /></div>
+      <Biographic />
+      <Skills />
+    </m.section>
   );
 }
 
---
+++ src/components/about/main/Skills.jsx
@@ -0,0 +1,30 @@
+import classes from './Skills.module.scss';
+import js__logo from '../../../assets/javascript.svg';
+import react__logo from '../../../assets/react.svg';
+import mongodb__logo from '../../../assets/mongodb.svg';
+import angular__logo from '../../../assets/angular.svg';
+
+const Skills = () => {
+
+    return (
+        <div className={classes.logos}>
+            <h2><span>Skills</span></h2>
+            <ul>
+                <li>
+                    <img src={js__logo} alt="javascript logo" />
+                </li>
+                <li>
+                    <img src={react__logo} alt="react logo" />
+                </li>
+                <li>
+                    <img src={mongodb__logo} alt="mongodb logo" />
+                </li>
+                <li>
+                    <img src={angular__logo} alt="angular logo" />
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Skills;
