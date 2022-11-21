7f4d6054ebc9efe43deccf82ec31343b30da4145
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-21 17:19:57 +0100

    update

--- src/components/about/main/Biographic.jsx
+++ src/components/about/main/Biographic.jsx
@@ -8,11 +8,11 @@ const Biographic = () => {
 
     return (
         <article className={classes.bio}>
-            <div className={classes.about__me}>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            </div>
-            <div>
-                <h2 className={classes.skills}><span>Skills</span></h2>
+            <p className={classes.about__me}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div className={classes.skills}>
+                <h2><span>Skills</span></h2>
                 <div className={classes.logos}>
                     <ul>
                         <li>
