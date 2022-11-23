76a6a362b3277b61d2d36ed1778ea31551706981
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-23 22:26:06 +0100

    update

--- src/nav_2/Menu.jsx
+++ src/nav_2/Menu.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import classes from "./Menu.module.scss";
 
 const Menu = () => {
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const menuHandler = () => {
+        setIsOpen(prev => !prev);
+    }
+
+    const open = isOpen ? classes.open : undefined;
+    const classesArray = [classes.hamburger, open];
+
     return (
-        <button type="button" className={classes.hamburger}></button>
+        <button type='button' className={classesArray.join(' ')} onClick={menuHandler}>
+            <div>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+        </button>
     );
 }
 
