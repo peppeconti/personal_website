ce5b38d6ad9d67e490d8648889be5a1d57597388
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-24 11:29:13 +0100

    update

---
+++ src/nav_2/Modal.jsx
@@ -0,0 +1,50 @@
+import { useId } from 'react';
+import { createPortal } from 'react-dom';
+import { useLocation } from 'react-router-dom';
+import classes from "./Menu.module.scss";
+import { motion as m, AnimatePresence } from 'framer-motion';
+import LinkIcon from './LinkIcon';
+import home from '../assets/home.svg'
+import envelope from '../assets/envelope-solid.svg';
+import info from '../assets/circle-info-solid.svg';
+import laptop from '../assets/laptop-code-solid.svg';
+
+const Modal = () => {
+
+    const location = useLocation();
+
+    const icons = [
+        {
+            to: '/about',
+            icon: info,
+            alt: 'about',
+            id: useId()
+        },
+        {
+            to: '/portfolio',
+            icon: laptop,
+            id: useId()
+        },
+        {
+            to: '/contact',
+            icon: envelope,
+            id: useId()
+        }
+    ];
+
+    const active = icons.filter(e => e.to === location.pathname);
+    const inactive = icons.filter(e => e.to !== location.pathname);
+
+    const content = <m.nav className={classes.modal} exit={{ opacity: 0 }} transition={{ duration: 1 }}>
+        <ul>
+            <LinkIcon key={useId()} href={'/'} source={home} alt="home" isRoute />
+            {active.map(e => <LinkIcon key={e.id} href={e.to} source={e.icon} alt={e.alt} isRoute />)}
+            {inactive.map((e, i) => <LinkIcon key={e.id} href={e.to} source={e.icon} alt={e.alt} isRoute />)}
+        </ul>
+    </m.nav>
+
+    return createPortal(<AnimatePresence>{content}</AnimatePresence>, document.getElementById('overlay'));
+
+}
+
+export default Modal;
