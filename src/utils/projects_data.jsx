8eb6e5fd4f35827255bd8dccdcb41df29567e606
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-29 16:48:29 +0100

    added datas obj

--- src/utils/projects_data.jsx
+++ src/utils/projects_data.jsx
@@ -1,6 +1,30 @@
+import { m } from 'framer-motion';
+import connect__4 from '../assets/connect-4.jpg';
+
 export const data = [
-    {name: 'Connect 4'},
-    {name: 'Simon'},
-    {name: 'HappyChat'},
-    {name: 'Liber der Introductiione Loquendi'}
+    {
+        name: 'Connect 4',
+        img: connect__4,
+        description: 'lalallalalallalall',
+        url: 'https://connect-four-4b5ca.web.app/'
+
+    },
+    {
+        name: 'Simon',
+        img: connect__4,
+        description: 'lalafkfdknjeed',
+        url: 'https://connect-four-4b5ca.web.app/'
+    },
+    {
+        name: 'HappyChat',
+        img: connect__4,
+        description: 'jfjfjfncdwljjnd',
+        url: 'https://divine--comedy-digital.firebaseapp.com/inferno/inferno-1'
+    },
+    {
+        name: 'Liber der Introductione Loquendi',
+        img: connect__4,
+        description: 'lrordkjvn847474u4',
+        url: 'https://divine--comedy-digital.firebaseapp.com/inferno/inferno-1'
+    }
 ];
