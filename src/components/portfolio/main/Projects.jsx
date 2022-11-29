2d8f88b81dc827ac767040c7214e06e5800a6b32
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-11-29 14:29:09 +0100

    update

---
+++ src/components/portfolio/main/Projects.jsx
@@ -0,0 +1,12 @@
+import classes from './Projects.module.scss';
+import connect_4 from '../../../assets/connect-4.jpg';
+
+const Projects = () => {
+
+    return (
+        <div className={classes.projects}>
+        </div>
+    );
+}
+
+export default Projects;
