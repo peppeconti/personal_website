fa7cca062101d4703924cce3acc09938b1917702
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-12-06 18:10:25 +0100

    update

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -1,10 +1,17 @@
 import classes from './Main.module.scss';
+import laptop from '../../assets/laptop.svg';
 
 const Main = () => {
 
   return (
     <main className={classes.main}>
-      Ciccio
+      <div>
+        <h1>Giuseppe Conti<br/>Web Developer<br/>Milan</h1>
+        <h2>Front End Developer / Milan</h2>
+      </div>
+      <figure>
+        <img src={laptop} alt='laptop' />
+      </figure>
     </main>
   );
 }
