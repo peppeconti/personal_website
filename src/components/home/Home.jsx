513b8e6656496f20f4d83e06d9183446ed0d72d4
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-12-07 13:30:04 +0100

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,12 @@
 import classes from './Home.module.scss';
-// import Nav from './Nav';
+import Nav from './Nav';
 import Main from './Main';
 
 const Home = () => {
 
   return (
     <header className={classes.home}>
+      <Nav />
       <Main />
     </header>
   );
--- src/components/home/Nav.jsx
+++ src/components/home/Nav.jsx
@@ -5,9 +5,9 @@ const Nav = () => {
     return (
         <nav className={classes.nav}>
             <ul>
-                <li><span>a</span>bout</li>
-                <li><span>w</span>ork</li>
-                <li><span>c</span>ontact</li>
+                <a href='#about'><li><span>a</span>bout</li></a>
+                <a href='#work'><li><span>w</span>ork</li></a>
+                <a href='#contact'><li><span>c</span>ontact</li></a>
             </ul>
         </nav>
     );
