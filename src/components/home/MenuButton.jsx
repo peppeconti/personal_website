72d45d89ba74d22e01916765c6f1829e2ac23055
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-12-08 22:31:36 +0100

    update

--- src/components/home/MenuButton.jsx
+++ src/components/home/MenuButton.jsx
@@ -10,17 +10,80 @@ const MenuButton = () => {
         setOpen(prev => !prev)
     }
 
+    const flex = {
+        open: {
+            justifyContent: 'space-around',
+            rotateZ: '270deg',
+            transition: {
+                duration: .5
+            }
+        },
+        close: {
+            justifyContent: 'space-around',
+            rotateZ: '0deg',
+            transition: {
+                duration: .5
+            }
+        }
+    }
+
+    const grow = [{
+        open: {
+            translateX: '1.2rem',
+            width: '10%',
+            transition: {
+                duration: .5
+            }
+        },
+        close: {
+            translateX: '0rem',
+            width: '70%',
+            transition: {
+                duration: .5
+            }
+        }
+    },{
+        open: {
+            translateX: '1.2rem',
+            width: '10%',
+            transition: {
+                duration: .5
+            }
+        },
+        close: {
+            translateX: '0rem',
+            width: '40%',
+            transition: {
+                duration: .5
+            }
+        }
+    },{
+        open: {
+            translateX: '1.2rem',
+            width: '10%',
+            transition: {
+                duration: .5
+            }
+        },
+        close: {
+            translateX: '0rem',
+            width: '90%',
+            transition: {
+                duration: .5
+            }
+        }
+    }]
+
     return (
         <motion.button
-            layout
             className={classes.menu}
             onClick={menuHandler}
-            animate={open ? { rotateZ: 90 } : { rotateZ: 0 }}
-            data-open={open}
+            variants={flex}
+            animate={open ? 'open' : 'close'}
         >
-            <motion.div layout className={classes.line} animate={open ? { width: '5px', margin: '0' } : { width: '70%', margin: '4px 0' }}></motion.div>
-            <motion.div layout className={classes.line} animate={open ? { width: '5px', margin: '0' } : { width: '40%', margin: '4px 0' }}></motion.div>
-            <motion.div layout className={classes.line} animate={open ? { width: '5px', margin: '0' } : { width: '90%', margin: '4px 0' }}></motion.div>
+            <motion.div className={classes.line} variants={grow[0]}></motion.div>
+            <motion.div className={classes.line} variants={grow[1]}></motion.div>
+            <motion.div className={classes.line} variants={grow[2]}></motion.div>
         </motion.button>
     );
 }
