91b6481bcb2a1aa6804c77fd4b5bbd4e8f7a70bf
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-12-08 20:22:38 +0100

    update animation

--- src/components/home/MenuButton.jsx
+++ src/components/home/MenuButton.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import classes from './MenuButton.module.scss';
+import { motion } from 'framer-motion';
 
 const MenuButton = () => {
 
@@ -9,13 +10,13 @@ const MenuButton = () => {
         setOpen(prev => !prev )
     }
 
-    const classesList = open ? classes.line + ' ' + classes.point : classes.line;
+    //const classesList = open ? classes.line + ' ' + classes.point : classes.line;
 
     return (
         <button className={classes.menu} onClick={menuHandler}>
-            <div className={classesList}></div>
-            <div className={classesList}></div>
-            <div className={classesList}></div>
+            <motion.div className={classes.line} animate={open ? { width: '5px'} : { width: '70%'} }></motion.div>
+            <motion.div className={classes.line} animate={open ? { width: '5px'} : { width: '40%'} }></motion.div>
+            <motion.div className={classes.line} animate={open ? { width: '5px'} : { width: '90%'} }></motion.div>
         </button>
     );
 }
