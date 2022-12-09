9bc213d4eb46df9879d0388c2cb03a9d036b312e
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-12-09 14:18:31 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './components/home/Home';
+import About from './components/about/About';
 
 function App() {
 
   return (
     <React.Fragment>
       <Home />
+      <About />
     </React.Fragment>
   );
 }
---
+++ src/components/about/About.jsx
@@ -0,0 +1,12 @@
+import classes from './About.module.scss';
+
+const About = () => {
+
+    return (
+        <section className={classes.about}>
+           ciao
+        </section>
+    );
+}
+
+export default About;
