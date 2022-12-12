d241b2aff4385d306825ec9ee8c19d393e49fc4d
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-12-12 15:15:35 +0100

    update

--- src/components/about/Infos.jsx
+++ src/components/about/Infos.jsx
@@ -1,15 +1,14 @@
 import { useRef } from 'react';
 import classes from './Infos.module.scss';
-import about from '../../assets/laptop.svg';
 import { motion, useScroll } from 'framer-motion';
 
 const Infos = () => {
 
     return (
         <article className={classes.infos}>
-            <div className={classes.image}>
-                <img src={about} alt='about me'/>
-            </div>
+            <div
+                className={classes.image}
+            >About me</div>
             <div className={classes.curriculum}></div>
         </article>
     );
