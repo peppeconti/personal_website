d3aee11088ad0c0104f36d9d1e671314b498edc9
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-12-14 23:36:56 +0100

    update

--- src/components/about/Skills.jsx
+++ src/components/about/Skills.jsx
@@ -1,5 +1,6 @@
 import classes from './Skills.module.scss';
 import SingleSkill from './SingleSkill';
+import { data as skills } from '../../utils/skills';
 
 const Skills = () => {
 
@@ -7,14 +8,7 @@ const Skills = () => {
         <article className={classes.skills}>
             <h2>Skills</h2>
             <div className={classes.skill__list}>
-                <SingleSkill name='HTML/CSS' percentage='80%' />
-                <SingleSkill name='JavaScript' percentage='70%' />
-                <SingleSkill name='React' percentage='60%' />
-                <SingleSkill name='TypeScript' percentage='30%' />
-                <SingleSkill name='Angular' percentage='40%' />
-                <SingleSkill name='MongoDB' percentage='40%' />
-                <SingleSkill name='NodeJS' percentage='40%' />
-                <SingleSkill name='Figma' percentage='45%' />
+                {skills.map(e => <SingleSkill key={e.id} name={e.name} percentage={e.percentage} />)}
             </div>
         </article>
     );
---
+++ src/utils/skills.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        id: 1,
+        name: 'HTML/CSS',
+        percentage: '80%',
+    },
+    {
+        id: 2,
+        name: 'JavaScript',
+        percentage: '70%',
+    },
+    {
+        id: 3,
+        name: 'React',
+        percentage: '70%',
+    },
+    {
+        id: 4,
+        name: 'TypeScript',
+        percentage: '30%',
+    },
+    {
+        id: 5,
+        name: 'Angular',
+        percentage: '40%',
+    },
+    {
+        id: 6,
+        name: 'MongoDB',
+        percentage: '40%',
+    },
+    {
+        id: 7,
+        name: 'NodeJS',
+        percentage: '40%',
+    },
+    {
+        id: 8,
+        name: 'Figma',
+        percentage: '45%',
+    }
+]
