b168e91d3b73bce5a3d0ae07237a98f3f737e4c9
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-12-15 20:55:47 +0100

    update

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -3,19 +3,25 @@ export const data = [
         id: 1,
         name: 'Connect 4',
         description: 'A version of the classical game',
-        url: 'https://connect-four-4b5ca.web.app/'
+        keywords: ['2D Array', 'React'],
+        url: 'https://connect-four-4b5ca.web.app/',
+        repository: 'https://github.com/peppeconti/connect-4'
 
     },
     {
         id: 2,
         name: 'Simon',
         description: 'A React version of the vintage memory game',
-        url: 'https://connect-four-4b5ca.web.app/'
+        keywords: ['Memory Game', 'React'],
+        url: 'https://connect-four-4b5ca.web.app/',
+        repository: 'https://github.com/peppeconti/simon'
     },
     {
         id: 3,
         name: 'Liber de introductione loquendi',
-        description: 'A digital edition of the manuscript Clm 16126 of the Bayerische Staatbibliothek of Munich, based on the TEI XML format',
-        url: 'https://divine--comedy-digital.firebaseapp.com/inferno/inferno-1'
+        description: 'A digital edition of the manuscript Clm 16126 of the Bayerische Staatsbibliothek of Munich based on the TEI-XML format',
+        keywords: ['Digital Humanities', 'Angular', 'TEI-XML'],
+        url: 'https://divine--comedy-digital.firebaseapp.com/inferno/inferno-1',
+        repository: 'https://github.com/peppeconti/liber_de_introductione_loquendi'
     },
 ];
