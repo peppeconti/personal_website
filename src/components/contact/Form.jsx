94841ec882051fe60a5d0646e7adcb85aaab95cf
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-12-16 18:04:01 +0100

    update

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -23,13 +23,23 @@ const Form = () => {
 
     return (
         <form ref={form} onSubmit={sendEmail} className={classes.form}>
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
+            <div>
+                <div className={classes.group}>
+                    <label htmlFor='user_name'>Name</label>
+                    <input type='text' name='user_name' />
+                </div>
+                <div className={classes.group}>
+                    <label htmlFor='user_email'>Email</label>
+                    <input type='email' name='user_email' />
+                </div>
+            </div>
+            <div className={classes.group}>
+                <label htmlFor='message'>Message</label>
+                <textarea name='message' />
+            </div>
+            <div className={classes.group}>
+                <button type='submit'>Submit</button>
+            </div>
         </form>
 
     );
