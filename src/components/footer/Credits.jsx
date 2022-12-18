8272db452fecac7c80203c656f22b32d76fde537
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-12-18 18:19:52 +0100

    added footer

--- src/components/footer/Credits.jsx
+++ src/components/footer/Credits.jsx
@@ -1,10 +1,14 @@
 import classes from './Credits.module.scss';
+import { data as social } from '../../utils/social';
 
 const Credits = () => {
 
+    const fullYear = new Date().getFullYear();
+
     return (
         <div className={classes.credits}>
-
+            <h4 className={classes.copyright}>&#169; <time>{fullYear}</time> Copyright Giuseppe Conti</h4>
+            <ul className={classes.social}></ul>
         </div>
     );
 }
---
+++ src/utils/social.js
@@ -0,0 +1,24 @@
+import linkedin from '../assets/linkedin-in.svg';
+import github from '../assets/github.svg';
+import codewars from '../assets/cib-codewars.svg';
+
+export const data = [
+    {
+        id: 1,
+        name: 'linkedin',
+        logo: linkedin,
+        url: 'https://www.linkedin.com/in/giuseppe-conti-4a806b16/',
+    },
+    {
+        id: 2,
+        name: 'github',
+        logo: github,
+        url: 'https://github.com/peppeconti',
+    },
+    {
+        id: 3,
+        name: 'codewars',
+        logo: codewars,
+        url: 'https://github.com/peppeconti',
+    }
+]
