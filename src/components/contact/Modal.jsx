a61f2280969e297d114122cadc80c31220392e62
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-12-18 22:30:25 +0100

    update

--- src/components/contact/Modal.jsx
+++ src/components/contact/Modal.jsx
@@ -1,6 +1,7 @@
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import classes from './Modal.module.scss';
+import { motion as m } from 'framer-motion';
 
 const Backdrop = ({ setModalOpen, resetMessage, unLockScroll }) => {
     return (
@@ -18,10 +19,38 @@ const Backdrop = ({ setModalOpen, resetMessage, unLockScroll }) => {
 
 const Message = ({ message }) => {
 
+    const rotate = {
+        squareA: { 
+            x: [0, 35, 35, 0, 0],
+            y: [0, 0, 35, 35, 0],
+            scale: [1, .8, 1, .8, 1],
+            transition: {
+                type: 'spring', 
+                duration: 1.2,
+                repeat: Infinity
+            }
+        },
+        squareB: { 
+            x: [0, -35, -35, 0, 0],
+            y: [0, 0, -35, -35, 0],
+            scale: [1, .8, 1, .8, 1],
+            transition: {
+                type: 'spring',
+                duration: 1.2,
+                repeat: Infinity
+            }
+        }
+    }
+
+    const loader = <p className={classes.loader}>
+        <m.span variants={rotate} animate='squareA'  className={classes.squareA}/>
+        <m.span variants={rotate} animate='squareB' className={classes.squareB}/>
+    </p>
+
     return (
         <div className={classes.message}>
             <p className={classes.confirm}>
-                {message ? message : '...wait...'}
+                {message ? message : loader}
             </p>
         </div>
     );
