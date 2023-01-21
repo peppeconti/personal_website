4b322901356be6e66b7c1399fd64bee194437e4a
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-01-21 23:03:05 +0100

    update

--- src/components/showroom/Project.jsx
+++ src/components/showroom/Project.jsx
@@ -29,7 +29,6 @@ const Project = ({ title, subtitle, logo, description, url, repository, keywords
 
     return (
         <article ref={scrollRef} className={classes.project}>
-            <div className={classes.extension} />
             <m.div style={{ x: openLeft }} viewport={{ root: scrollRef }} className={classes.description}>
                 <figure className={classes.repository}>
                     <a href={repository} target='_blank' rel='noreferrer'>
@@ -52,3 +51,5 @@ const Project = ({ title, subtitle, logo, description, url, repository, keywords
 }
 
 export default Project;
+
+//<div className={classes.extension} />
