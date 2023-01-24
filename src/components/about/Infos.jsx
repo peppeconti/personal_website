e83821034fa31a920869608cea3e2b39047be04f
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-01-24 17:33:16 +0100

    text update

--- src/components/about/Infos.jsx
+++ src/components/about/Infos.jsx
@@ -5,7 +5,7 @@ const Infos = () => {
     return (
         <article className={classes.infos}>
             <h1>About me</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium euismod magna a viverra. Vivamus lobortis viverra quam, rhoncus pellentesque sapien iaculis eget. Maecenas urna urna, lobortis vel ultricies sed, tincidunt sed mauris. Cras hendrerit justo ipsum, eu mollis est dapibus vel. Integer ac tellus vel nisl faucibus gravida. Vestibulum euismod ipsum vel hendrerit varius. Praesent non iaculis libero. Vestibulum ut leo vel arcu viverra aliquam vel ut enim. Nunc ut felis tincidunt, cursus quam in, fermentum turpis. Suspendisse in enim ut ipsum scelerisque auctor. Ut urna nisl, elementum sed gravida sit amet, bibendum eget est.</p>
+            <p>I started programming when I was 8 writing "spaghetti code" with my C64 to solve math problems involving square gardens with round flowerbeds. There was no internet then, state TV only broadcast three channels, and video games were portrayed as evil. So I had to wait several years to get back into programming (and here you can see the results).</p>
         </article>
     );
 }
