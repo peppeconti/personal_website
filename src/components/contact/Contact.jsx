20d4e3c9403d3aa3871e5a2a97375504cfd23cb3
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-01-26 12:31:37 +0100

    update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import classes from './Contact.module.scss';
 import Form from './Form';
+import { scrollIntoView } from "seamless-scroll-polyfill";
 
 const Contact = () => {
+
+  useEffect(() => {
+    scrollIntoView(document.querySelector("#target"), { behavior: "smooth", block: "center", inline: "center" });
+  },[]) 
+
 
   return (
     <section id='contact' className={classes.contact}>
