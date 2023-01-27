2de0d46f84702a63434988d968aad76cd37affde
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-01-27 15:01:14 +0100

    update

--- src/components/showroom/Project.jsx
+++ src/components/showroom/Project.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { Fragment, useRef } from 'react';
 import classes from './Project.module.scss';
 import { motion as m, useScroll, useTransform } from 'framer-motion';
 import github from '../../assets/github__repository.svg';
@@ -39,27 +39,33 @@ const Project = ({ title, subtitle, logo, font, description, url, repository, ke
 
 
     return (
-        <article ref={scrollRef} className={classes.project}>
-            <div className={classes.extension} />
-            <m.div style={{ x: openLeft }} viewport={{ root: scrollRef }} className={classes.description}>
-                <figure className={classes.repository}>
-                    <a href={repository} target='_blank' rel='noreferrer'>
-                        <img width={30} height={30} src={github} alt='github repository' />
-                    </a>
-                </figure>
-                <div>
-                    <p className={classes.text__description}>{description}</p>
-                    <p className={classes.keywords}>{keywordsList}</p>
-                </div>
-            </m.div>
-            <m.a href={url} target='_blank' style={{ x: openRight }} viewport={{ root: scrollRef }} className={classes.name}>
-                <div className={classes.title}>
-                    <span className={classes.logo}><img src={logo} alt={title.text}/></span>
-                    <h3 style={titleStyle}>{title.text}</h3>
-                    <h4 style={subtitleStyle}>{subtitle.text}</h4>
-                </div>
-            </m.a>
-        </article>
+        <Fragment>
+            <div className={classes.title}>
+                <h3 style={titleStyle}>{title.text}</h3>
+                <span className={classes.logo}><img src={logo} alt={title.text} /></span>
+            </div>
+            <article ref={scrollRef} className={classes.project}>
+                <div className={classes.extension} />
+                <m.div style={{ x: openLeft }} viewport={{ root: scrollRef }} className={classes.description}>
+                    <figure className={classes.repository}>
+                        <a href={repository} target='_blank' rel='noreferrer'>
+                            <img width={30} height={30} src={github} alt='github repository' />
+                        </a>
+                    </figure>
+                    <div>
+                        <p className={classes.text__description}>{description}</p>
+                        <p className={classes.keywords}>{keywordsList}</p>
+                    </div>
+                </m.div>
+                <m.a href={url} target='_blank' style={{ x: openRight }} viewport={{ root: scrollRef }} className={classes.name}>
+                    <div className={classes.title}>
+                        <span className={classes.logo}><img src={logo} alt={title.text} /></span>
+                        <h3 style={titleStyle}>{title.text}</h3>
+                        <h4 style={subtitleStyle}>{subtitle.text}</h4>
+                    </div>
+                </m.a>
+            </article>
+        </Fragment>
     );
 }
 
