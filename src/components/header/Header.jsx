cd12fff828145cd9090fe10d64ffa4937d79bdf1
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-04-04 21:23:04 +0200

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,15 @@
-import { useRef } from 'react';
 import classes from "./Header.module.scss";
 import Hero from "./Hero";
 import Nav from "./Nav";
 
 const Header = () => {
 
-  const headerRef = useRef();
-
-  const setHeight = () => {
-   headerRef.style.minHeight =
-      window.innerHeight + "px";
+  const setHeight = {
+    minHeight: `${window.innerHeight}px`
   };
 
-
-  let deviceWidth = window.matchMedia("(max-width: 1024px)");
-
-  if (deviceWidth.matches) {
-
-    window.addEventListener("resize", setHeight);
-
-    setHeight();
-  }
-
   return (
-    <header ref={headerRef} className={classes.header}>
+    <header style={setHeight} className={classes.header}>
       <Hero />
       <Nav />
     </header>
