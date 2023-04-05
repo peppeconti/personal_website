2a481c5a07b9400ac4089ea3b5eb1a43efb645ef
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-04-05 19:30:46 +0200

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,21 @@
 import classes from "./Header.module.scss";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 import Hero from "./Hero";
 import Nav from "./Nav";
 
 const Header = () => {
+  const mobile = useMediaQuery("max-width: 1024px");
+
+  let minHeight;
+
+  if (mobile) {
+    height = `${window.innerHeight}px`;
+  } else {
+    height = "100vh";
+  }
 
   const setHeight = {
-    minHeight: `${window.innerHeight}px`
+    minHeight: minHeight,
   };
 
   return (
